e605d4489b6687113c4ce88101f1eabfb73e796c
steve.bealer <stevebealer@gmail.com>
2016-12-01 23:16:44 -0800

    Added seperate function for call from CRON

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
-
-#from __future__ import absolute_import, division, print_function, unicode_literals
-
-import sys
 import time
 import os
 import json
-
-from flask import Flask, Response, request
-
+import requests
+from flask import Flask, Response
 from selenium.webdriver import PhantomJS
 from selenium.webdriver import DesiredCapabilities
 import selenium.webdriver.support.ui as ui
 from selenium.common.exceptions import TimeoutException
 
 app = Flask(__name__)
+
+#########################################
+debug=False
+#########################################
+
+hipchat_url = 'https://marketfleet.hipchat.com/v2/room/3345643/notification?auth_token=COcvKHKwMGYD509WhwesCqv9KjW5fHGDtDokP4vz'
+hipchat_url = 'https://build.hipchat.com/v2/room/2703095/notification?auth_token=6CtGAdfHLnOzfknWh3Os1T74aTc3mIqGusAbVBrL'
+
+good_note = {"color":"green","notify":'false',"message_format":"text"}
+bad_note = {'color': 'red', 'notify': 'false', 'message_format': 'text'}
+
+def prnt(text,always_print=False):
+    if debug or always_print:
+        prnt(text)
 
 @app.route("/")
 def default():
@@ -23,63 +32,75 @@ def default():
 @app.route("/sales", methods=['GET','POST'])
 def sales():
 
+    return Response(get_sales())
+
+#######################################################################################################################
+
+def send_hipchat_note():
+    print "sending data"
+    sales_data = get_sales()
+    requests.post(hipchat_url, data=sales_data,headers={"Content-Type":"application/json"})
+
+#######################################################################################################################
+
+def get_sales():
+
     dcap = dict(DesiredCapabilities.PHANTOMJS)
     dcap["phantomjs.page.settings.userAgent"] = (
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36")
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36")
 
-    test_url = "http://requestb.in/18h1dbl1"
     login_url = "https://sellercentral.amazon.com/gp/homepage.html"
     report_url = "https://sellercentral.amazon.com/gp/site-metrics/report.html#&reportID=eD0RCS"
 
     if os.name == 'nt':
         phantomjs_path = "C:\Python27\phantomjs.exe"
         browser = PhantomJS(executable_path=phantomjs_path, desired_capabilities=dcap)
-        print("Browser started.")
+        prnt("Browser started.", True)
 
     else:
         browser = PhantomJS(desired_capabilities=dcap)
-        print("Created")
+        prnt("Browser started.", True)
 
-    #browser.set_page_load_timeout(10)
-    #browser.implicitly_wait(10)
+    # browser.set_page_load_timeout(10)
+    # browser.implicitly_wait(10)
     wait = ui.WebDriverWait(browser, 20)
 
     try:
 
         browser.get(login_url)
-        #time.sleep(2)
+        # time.sleep(2)
 
         wait.until(lambda browser_find: browser.find_element_by_id("signInSubmit"))
-        print("Found login page.")
-        #wait.until(lambda browser_find: browser.find_element_by_id("ap_email"))
-        print("Finding ap_email")
+        prnt("Found login page.")
+        # wait.until(lambda browser_find: browser.find_element_by_id("ap_email"))
+        prnt("Finding ap_email")
         username = browser.find_element_by_id("ap_email")
         password = browser.find_element_by_id("ap_password")
-        print("Found both fields. Gathering credentials.")
+        prnt("Found both fields. Gathering credentials.")
         auth1 = os.environ["AZN_AUTH1"]
         auth2 = os.environ["AZN_AUTH2"]
-        print("Got Credentials. Attempting to send keys.")
-        print(auth1)
+        prnt("Got Credentials. Attempting to send keys.")
+        prnt(auth1)
         username.send_keys(auth1)
-        print("username entered.")
+        prnt("username entered.")
         password.send_keys(auth2)
         time.sleep(1)
-        print("password entered.")
-        print("Logging in.")
+        prnt("password entered.")
+        prnt("Logging in.")
         browser.find_element_by_id("signInSubmit").submit()
-        print("Logged in.")
-        #png = browser.get_screenshot_as_png()
+        prnt("Logged in.")
+        # png = browser.get_screenshot_as_png()
         # return Response(png, mimetype="image/png")
-        print("Trying to navigate to report url.")
+        prnt("Trying to navigate to report url.")
         browser.get(report_url)
-        print("Supposedly navigated...")
-        print(browser.title)
+        prnt("Supposedly navigated...")
+        prnt(browser.title)
         wait.until(lambda browser_find: browser.find_element_by_id("summaryOPS"))
-        print("Found summaryOPS element.")
+        prnt("Found summaryOPS element.")
         summary_val = browser.find_element_by_id("summaryOPS").text
-        print("Value was: {n}".format(n=summary_val))
+        prnt("Value was: {n}".format(n=summary_val))
         if summary_val == '0':
-            print("In loop")
+            prnt("In loop")
             for retry in range(1, 5):
                 if summary_val != '0':
                     break
@@ -89,38 +110,37 @@ def sales():
         if summary_val == 0:
             raise Exception("Could not get sales figures in time or sales were $0")
 
-        resp = {"color": "green","notify": "false","message_format": "text"}
-        #
-        resp["message"] = summary_val
-        print("Trying to return message.")
-        return Response(json.dumps(resp))
+        resp = good_note
+
+        resp["message"] = "Marketfleet hourly sales notification: " + summary_val
+        prnt("Trying to return message.")
+        return json.dumps(resp)
 
     except TimeoutException as te:
-        # png = browser.get_screenshot_as_png()
-        # return Response(png, mimetype="image/png")
 
-        return Response("Couldn't find desired value in specified time limit.")
+        return "Couldn't find desired value in specified time limit."
 
     except Exception as gen_err:
-        #print(gen_err.message)
-        return Response(json.dumps({"color": "red",
-            "message": "Crap. An error occurred. ",
-            "notify": "false",
-            "message_format": "text"}))
+        prnt(gen_err.message)
+        error_text = bad_note
+        error_text["message"] = "An error occurred: {e}".format(e=gen_err.message)
+
+        return json.dumps(error_text)
+
         # png = browser.get_screenshot_as_png()
         # return Response(png, mimetype="image/png")
 
     finally:
         browser.quit()
 
-    # png = browser.get_screenshot_as_png()
-    # browser.quit()
-    #
-    # return Response(png, mimetype="image/png")
+        # png = browser.get_screenshot_as_png()
+        # browser.quit()
+        #
+        # return Response(png, mimetype="image/png")
 
-
+#######################################################################################################################
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
 
 
