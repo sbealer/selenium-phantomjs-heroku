b6fe9c41f927501dfe40f10906dd014cca3df5bd
steve.bealer <stevebealer@gmail.com>
2016-12-06 12:03:19 -0800

    Added dev route

--- scheduled_sales_summary_note.py
+++ scheduled_sales_summary_note.py
@@ -2,7 +2,7 @@ import app
 
 def post_to_hipchat():
     print("Sending note to hipchat")
-    app.send_hipchat_note()
+    app.send_hipchat_note('reg')
 
 if __name__ == "__main__":
     post_to_hipchat()
